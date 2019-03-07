b13075af919dea07d2d7ed8f450c4c850979c7e5
Vitnija Bluzma <thepunkyone@gmail.com>
2019-03-07 19:44:58 +0000

    set up Ship constructor and startingPort

--- __tests__/cruise-ships.test.js
+++ __tests__/cruise-ships.test.js
@@ -1,1 +1,15 @@
 const Ship = require('../src/cruise-ships');
+
+describe('Ship constructor', () => {
+    let cruiseShip;
+    beforeEach(() => {
+        cruiseShip = new Ship('Liverpool');
+    });
+    it('cruiseShip is an object', () => {
+        expect(cruiseShip).toBeInstanceOf(Object);
+    });
+    it('cruiseShip has a starting port property', () => {
+        expect(typeof cruiseShip.startingPort).toBe('string');
+        expect(cruiseShip.startingPort).toBe('Liverpool');
+    });
+});
--- src/cruise-ships.js
+++ src/cruise-ships.js
@@ -1,4 +1,6 @@
-
+function Ship(startingPort) {
+    this.startingPort = startingPort;
+};
 
 
 
