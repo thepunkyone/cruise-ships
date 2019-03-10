41a255d92ac17197fc18cae07e3b262489dd1a49
Vitnija Bluzma <thepunkyone@gmail.com>
2019-03-10 18:54:12 +0000

    DRY refactored Itinerary module

--- __tests__/itinerary.test.js
+++ __tests__/itinerary.test.js
@@ -1,16 +1,11 @@
-const Port = require('../src/port');
 const Itinerary = require('../src/itinerary');
 
 describe('Itinerary constructor', () => {
-    let dover;
-    let calais;
-    let itinerary;
-    beforeEach(() => {
-        dover = new Port('Dover');
-        calais = new Port('Calais');
-        itinerary = new Itinerary([dover, calais]);
-    });
-    it('Itinerary has an starting port', () => {
+    const dover = jest.fn();
+    const calais = jest.fn();
+    const itinerary = new Itinerary([dover, calais]);
+
+    it('Itinerary has an array of ports', () => {
         expect(itinerary.ports).toEqual([dover, calais]);
     });
 });
