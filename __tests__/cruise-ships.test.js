2dd8d75bf4118a401c3f7a21d99d77890515fc34
Vitnija Bluzma <thepunkyone@gmail.com>
2019-03-10 15:11:17 +0000

    updated varables according to walkthrough

--- __tests__/cruise-ships.test.js
+++ __tests__/cruise-ships.test.js
@@ -3,22 +3,22 @@ const Port = require('../src/port');
 
 describe('Ship constructor', () => {
     let cruiseShip;
-    let port = new Port('Liverpool');
+    const dover = new Port('Dover');
     beforeEach(() => {
-        cruiseShip = new Ship(port);
+        cruiseShip = new Ship(dover);
     });
     it('cruiseShip is an object', () => {
         expect(cruiseShip).toBeInstanceOf(Object);
     });
     it('cruiseShip has a currentPort property', () => {
-        expect(cruiseShip.currentPort).toBe(port);
+        expect(cruiseShip.currentPort).toBe(dover);
     });
     it('cruiseShip has a setSail method', () => {
         cruiseShip.setSail();
         expect(cruiseShip.currentPort).toBeFalsy();
     });
     it('cruiseShip has a dock method', () => {
-        let calais = new Port('Calais');
+        const calais = new Port('Calais');
         cruiseShip.dock(calais);
         expect(cruiseShip.currentPort).toBeTruthy();
         expect(cruiseShip.currentPort).toEqual(calais);
